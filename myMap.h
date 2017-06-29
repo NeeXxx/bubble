6b3aeee2d7c6b3b52a3f03c8dce6bfb317daec50
NeeXxx <neexgzyx@163.com>
2017-06-29 16:27:24 +0800

    Create myMap.h

---
+++ myMap.h
@@ -0,0 +1,16 @@
+#ifndef MYMAP_H
+#define MYMAP_H
+
+#include "block.h"
+
+class myMap
+{
+    block content[10][10];
+public:
+    myMap();
+    block& blockAt(int x,int y) { return content[x][y]; }
+    void moveAToB(int x1,int y1,int x2,int y2);
+    void hideAToB(int x1,int y1,int x2,int y2);
+};
+
+#endif // MYMAP_H
